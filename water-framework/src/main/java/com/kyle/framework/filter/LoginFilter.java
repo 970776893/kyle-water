41459059a7047e6155ef3fbf22f37edf83657102
我叫张凯 <970776893@qq.com>
2018-06-05 23:34:06 +0800

    增加登录限制

--- water-framework/src/main/java/com/kyle/framework/filter/LoginFilter.java
+++ water-framework/src/main/java/com/kyle/framework/filter/LoginFilter.java
@@ -5,6 +5,9 @@ import com.kyle.framework.model.ModelResult;
 import com.kyle.framework.model.ReturnCodeEnum;
 import com.kyle.framework.utils.Constants;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,7 +15,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author -- zhangkai02
@@ -20,21 +27,65 @@ import java.io.IOException;
  */
 @Log4j2
 public class LoginFilter extends OncePerRequestFilter {
+
+    private Set<String> excludeFilters;
+
+    public void setExcludeConfigLocation(String configLocation) throws IOException {
+        File file = ResourceUtils.getFile(configLocation);
+        FileInputStream fileIn = new FileInputStream(file);
+        Properties properties = new Properties();
+        properties.load(fileIn);
+        excludeFilters = properties.stringPropertyNames();
+        fileIn.close();
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        HttpSession session = httpServletRequest.getSession();
-        Object userCode = session.getAttribute(Constants.LOGIN_SESSION_ATTRIBUTE_KEY);
-        System.out.println(httpServletRequest.getSession().getMaxInactiveInterval());
-        if(userCode == null){
+        boolean filter = doFilter(httpServletRequest);
+        if(!filter){
             log.info("未登录");
             ModelResult<String> noLoginResult = new ModelResult<>("未登录/登录已失效");
             noLoginResult.setCode(ReturnCodeEnum.NO_LOGIN.code);
             String message = JSON.toJSON(noLoginResult).toString();
             httpServletResponse.getOutputStream().write(message.getBytes(httpServletResponse.getCharacterEncoding()));
             httpServletResponse.setContentType("application/json");
-            httpServletRequest.getSession().setAttribute(Constants.LOGIN_SESSION_ATTRIBUTE_KEY, "1");
         }else{
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         }
+    }
+
+    private boolean doFilter(HttpServletRequest httpServletRequest) {
+        boolean isExclude = exculded(httpServletRequest);
+        if(isExclude) {
+            return true;
+        }
+        boolean isLogined = loggined(httpServletRequest);
+        if(isLogined) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private boolean loggined(HttpServletRequest httpServletRequest) {
+        HttpSession session = httpServletRequest.getSession();
+        Object userCode = session.getAttribute(Constants.LOGIN_SESSION_ATTRIBUTE_KEY);
+        return userCode != null;
+    }
+
+    private boolean exculded(HttpServletRequest httpServletRequest) {
+        if(CollectionUtils.isEmpty(excludeFilters)) {
+            return false;
+        }
+        String servletPath = httpServletRequest.getServletPath();
+        for(String excludeFileter : excludeFilters){
+            boolean isMatch = PatternMatchUtils.simpleMatch(excludeFileter, servletPath);
+            if(isMatch) {
+                return true;
+            }
+
+        }
+        return false;
     }
 }
